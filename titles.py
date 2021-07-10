3768cc6a03e394fdb65eeb312385c1477ceb579d
Zach Toogood <zrtoogood@gmail.com>
2021-07-10 18:42:29 +0300

    Output title data to server install

--- titles.py
+++ titles.py
@@ -1,6 +1,7 @@
 
 import xmltodict
 import xml.etree.ElementTree as ET
+from config import *
 from utils import *
 
 ############################
@@ -22,14 +23,26 @@ def titles():
         except:
             pass
 
-    with open("out/scripts/globals/titles.lua", "w") as file:
-        file.write("xi.title =\n{\n")
-        for key, value in titles.items():
-            cleaned_title = to_caps_string(value)
-            title_str = f"    {cleaned_title.ljust(longest_title_str - 1)}"
-            index_str = str(int(key))
-            utf8_comment = value.encode("utf-8")
+    # Generate output string
+    out_string = "xi.title =\n{\n"
+    for key, value in titles.items():
+        cleaned_title = to_caps_string(value)
+        title_str = f"    {cleaned_title.ljust(longest_title_str - 1)}"
+        index_str = str(int(key))
+        utf8_comment = value.encode("utf-8")
 
-            file.write(f"{title_str} = {index_str},\n")
-            #file.write(f"{title_str} = {index_str}, -- {utf8_comment}\n")
-        file.write("}\n")
+        out_string += f"{title_str} = {index_str},\n"
+        #file.write(f"{title_str} = {index_str}, -- {utf8_comment}\n")
+    out_string += "}\n"
+
+    with open("out/scripts/globals/titles.lua", "w") as file:
+        file.write(out_string)
+
+    server_filename = SERVER_DIR + "/scripts/globals/titles.lua"
+    with open(server_filename, 'r+') as server_file:
+        raw_server_data = server_file.read()
+        # Write to original files (only the text{...} table)
+        new_server_data = re.sub(r"(xi.title)(.|\n)*?(\}\n)", out_string, raw_server_data)
+        server_file.seek(0)
+        server_file.truncate()
+        server_file.write(new_server_data)
