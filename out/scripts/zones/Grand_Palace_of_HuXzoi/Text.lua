64ba46c480c1de41225d68781a12c7964105df00
claywar <claywar@beerlab.dev>
2022-12-18 11:06:01 -0500

    Update out for December 2022 Update

--- out/scripts/zones/Grand_Palace_of_HuXzoi/Text.lua
+++ out/scripts/zones/Grand_Palace_of_HuXzoi/Text.lua
@@ -1,12 +1,28 @@
     text =
     {
-        ITEM_CANNOT_BE_OBTAINED = 6384, -- You cannot obtain the <item>. Come back after sorting your inventory.
-        ITEM_OBTAINED           = 6390, -- Obtained: <item>.
-        GIL_OBTAINED            = 6391, -- Obtained <number> gil.
-        KEYITEM_OBTAINED        = 6393, -- Obtained key item: <keyitem>.
-        CARRIED_OVER_POINTS     = 7001, -- You have carried over <number> login point[/s].
-        LOGIN_CAMPAIGN_UNDERWAY = 7002, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
-        LOGIN_NUMBER            = 7003, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        CONQUEST_BASE           = 7079, -- Tallying conquest results...
-        HOMEPOINT_SET           = 7461, -- Home point set!
+        ITEM_CANNOT_BE_OBTAINED       = 6384, -- You cannot obtain the <item>. Come back after sorting your inventory.
+        ITEM_OBTAINED                 = 6390, -- Obtained: <item>.
+        GIL_OBTAINED                  = 6391, -- Obtained <number> gil.
+        KEYITEM_OBTAINED              = 6393, -- Obtained key item: <keyitem>.
+        CARRIED_OVER_POINTS           = 7001, -- You have carried over <number> login point[/s].
+        LOGIN_CAMPAIGN_UNDERWAY       = 7002, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
+        LOGIN_NUMBER                  = 7003, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        MEMBERS_LEVELS_ARE_RESTRICTED = 7023, -- Your party is unable to participate because certain members' levels are restricted.
+        CONQUEST_BASE                 = 7082, -- Tallying conquest results...
+        DOES_NOT_RESPOND              = 7246, -- The gate does not respond...
+        REQUEST_CONFIRMED             = 7367, -- Security portal access request confirmed. Commencing patrol routine. Stay on alert for intruder interference.
+        PATROL_COMPLETED              = 7368, -- Patrol routine completed. Request transfer of final security portal access duty. Awaiting confirmation.
+        DUTY_COMPLETE                 = 7369, -- Transfer of final security portal access duty complete.
+        TRANSFER_OF_ACCESS_DUTY       = 7370, -- Transfer of access duty complete.
+        RETURNING_TO_REGULAR_DUTIES   = 7371, -- Returning to regular duties.
+        PORTAL_EAST                   = 7372, -- You hear a portal open to the east...
+        PORTAL_WEST                   = 7373, -- You hear a portal open to the west...
+        PORTAL_NORTH                  = 7374, -- You hear a portal open to the north...
+        TIME_EXCEEDED                 = 7375, -- Patrol routine time restriction exceeded. Patrol aborted.
+        PATROL_SUSPENDED              = 7376, -- Patrol suspended. Awaiting orders.
+        RECOMMENCING_PATROL           = 7377, -- Recommencing patrol.
+        RECENTLY_ACTIVATED            = 7378, -- The alcove has recently been activated...
+        TIME_RESTRICTION              = 7379, -- Time restriction: <number> [minute/minutes] (Earth time)
+        QUASILUMIN_MAP_QUEST_OFFSET   = 7380, -- Warning. Chamber of Eventide accessed by unauthorized personnel, 4789209-980 increments previous.
+        HOMEPOINT_SET                 = 7464, -- Home point set!
     },
