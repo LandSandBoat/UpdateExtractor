e828d008aeb7fd34af3142c2fa5652b4e45eec3d
claywar <claywar@lsb.dev>
2024-08-25 14:33:28 -0400

    [output] August 2024 Version Update

--- out/scripts/zones/Crawlers_Nest_[S]/Text.lua
+++ out/scripts/zones/Crawlers_Nest_[S]/Text.lua
@@ -1,5 +1,6 @@
     text =
     {
+        CAMPAIGN_RESULTS_TALLIED      = 437,  -- Campaign results tallied.
         ITEM_CANNOT_BE_OBTAINED       = 6906, -- You cannot obtain the <item>. Come back after sorting your inventory.
         ITEM_OBTAINED                 = 6912, -- Obtained: <item>.
         GIL_OBTAINED                  = 6913, -- Obtained <number> gil.
@@ -8,6 +9,6 @@
         LOGIN_CAMPAIGN_UNDERWAY       = 7524, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
         LOGIN_NUMBER                  = 7525, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
         MEMBERS_LEVELS_ARE_RESTRICTED = 7545, -- Your party is unable to participate because certain members' levels are restricted.
-        ITEM_DELIVERY_DIALOG          = 7689, -- Hello! Any packages to sendy-wend?
-        COMMON_SENSE_SURVIVAL         = 8654, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        ITEM_DELIVERY_DIALOG          = 7692, -- Hello! Any packages to sendy-wend?
+        COMMON_SENSE_SURVIVAL         = 8657, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
     },
